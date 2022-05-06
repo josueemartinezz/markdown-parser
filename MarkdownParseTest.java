2c445a2826c55dc98cc93be14b830014109b3a59
josueemartinezz <103324696+josueemartinezz@users.noreply.github.com>
2022-05-06 13:31:11 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,11 +1,9 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 
 
@@ -16,12 +14,6 @@ public class MarkdownParseTest {
     }
     @Test
     public void testGetLinks() throws IOException{
-
-
-        //System.out.println(pathToFile.toAbsolutePath());
-
-        //assertEquals("hello", pathToFile.toAbsolutePath());
-        //C:\Users\Josue Martinez\AppData\Roaming\Code\User\workspaceStorage\88ee0edbf9197bdd8a912b5000d61c35\redhat.java\jdt_ws\markdown-parser_c2433895\test-file.md
 
         Path fileName = Path.of("test-file.md");
         String content = Files.readString(fileName);
